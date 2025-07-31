498dc240c6cfc0c1b0c1e62d1fa9f498163fb1cd
e-shiten-jp <86388761+e-shiten-jp@users.noreply.github.com>
2025-07-31 17:58:50 +0900

    Update e_api_get_kanougaku_shinyou_tel.py

--- e_api_get_kanougaku_shinyou_tel.py
+++ e_api_get_kanougaku_shinyou_tel.py
@@ -15,12 +15,9 @@
 #
 #
 # 利用方法: 
-# 事前に「e_api_login_tel.py」を実行して、仮想URL等を取得しておいてください。
-#
-#
-# 利用方法: 
 # 事前に「e_api_login_tel.py」を実行して、
 # 仮想URL（1日券）等を取得しておいてください。
+# 「e_api_login_tel.py」と同じディレクトリで実行してください。
 #
 #
 # == ご注意: ========================================
@@ -241,7 +238,20 @@ def func_read_from_file(str_fname):
                 str_read = str_read + line
         return str_read
     except IOError as e:
-        print('Can not Write!!!')
+        print('ファイルを読み込めません!!! ファイル名：',str_fname)
+        print(type(e))
+
+
+# 機能: ファイルに書き込む
+# 引数1: 出力ファイル名
+# 引数2: 出力するデータ
+# 備考:
+def func_write_to_file(str_fname_output, str_data):
+    try:
+        with open(str_fname_output, 'w', encoding = 'utf-8') as fout:
+            fout.write(str_data)
+    except IOError as e:
+        print('ファイルに書き込めません!!!  ファイル名：',str_fname_output)
         print(type(e))
 
 
@@ -380,19 +390,6 @@ def func_get_p_no(fname, class_login_property):
     class_login_property.p_no = int(json_p_no_info.get('p_no'))
         
     
-# 機能: ファイルに書き込む
-# 引数1: 出力ファイル名
-# 引数2: 出力するデータ
-# 備考:
-def func_write_to_file(str_fname_output, str_data):
-    try:
-        with open(str_fname_output, 'w', encoding = 'utf-8') as fout:
-            fout.write(str_data)
-    except IOError as e:
-        print('Can not Write!!!')
-        print(type(e))
-
-
 # 機能: p_noを保存するためのjson形式のテキストデータを作成します。
 # 引数1: p_noを保存するファイル名（fname_info_p_no = "e_api_info_p_no.txt"）
 # 引数2: 保存するp_no
@@ -480,7 +477,7 @@ def func_kanougaku_shinyou(int_p_no, class_login_property):
 # ======================================================================================================
 
 if __name__ == "__main__":
-    # --- ファイル名等を設定 ------------------------------------------------------------------
+    # --- ファイル名等を設定（実行ファイルと同じディレクトリ） ---------------------------------------
     fname_account_info = "e_api_account_info.txt"
     fname_login_response = "e_api_login_response.txt"
     fname_info_p_no = "e_api_info_p_no.txt"
@@ -527,3 +524,4 @@ if __name__ == "__main__":
     print()    
     # "p_no"を保存する。
     func_save_p_no(fname_info_p_no, my_login_property.p_no)
+       
